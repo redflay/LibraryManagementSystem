9cc7846f9c60545d03e524c4d9f7b50d300bd8d9
Александр Фомин <redflaygo@gmail.com>
2025-06-27 19:32:00 +0300

    Добавьте файлы проекта.

---
+++ LibraryManagementSystem/LibraryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryManagementSystem
+{
+    // the main class for all library elements that can display information
+    public abstract class LibraryItem
+    {
+        public Guid Id { get; protected set; } // protected set so that it can be set in child classes or constructor
+
+        public LibraryItem() // generate a unique ID upon creation
+        {
+            Id = Guid.NewGuid();
+        }
+        // virtual method for default using or overwriting
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}");
+        }
+    }
+}
